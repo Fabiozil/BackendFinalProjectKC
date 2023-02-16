72d38f6ee2dd173005a97f4722755bb228155eb0
Fabiozil <fabeac1999_1999@hotmail.com>
2023-02-15 20:22:59 -0500

    Dependencies fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalFilters(new HttpExceptionFilter());
     app.enableVersioning({
         type: VersioningType.URI,
     });
