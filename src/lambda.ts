e74f9bf05d589695d9326897cab12a82c93f4777
Fabiozil <fabeac1999_1999@hotmail.com>
2023-03-22 17:49:54 -0500

    Final commit

--- src/lambda.ts
+++ src/lambda.ts
@@ -43,11 +43,13 @@ async function bootstrapServer(): Promise<Server> {
 }
 
 export const handler: Handler = async (event: any, context: Context) => {
-    if (
-        event.body &&
-        event.headers["Content-Type"].includes("multipart/form-data")
-    ) {
-        event.body = Buffer.from(event.body, "binary") as unknown as string;
+    if (event.headers["Content-Type"]) {
+        if (
+            event.body &&
+            event.headers["Content-Type"].includes("multipart/form-data")
+        ) {
+            event.body = Buffer.from(event.body, "binary") as unknown as string;
+        }
     }
     cachedServer = await bootstrapServer();
     return proxy(cachedServer, event, context, "PROMISE").promise;
